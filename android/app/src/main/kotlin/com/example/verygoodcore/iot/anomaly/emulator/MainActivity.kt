63dcfe9859aa0e4a32bcff59c08bf6f2d59a2081
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-08-20 17:58:29 +0200

    [FEA] Implemented latest version of riverpod (2.6.1) for state management

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/iot/anomaly/emulator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verygoodcore.iot_anomaly_emulator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:iot_anomaly_emulator/app/app.dart';
+import 'package:iot_anomaly_emulator/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
