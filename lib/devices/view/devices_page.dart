cfab02c38de8dc3081ebf615c7833349c87ec7f0
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-08-21 23:30:27 +0200

    [FEA] Created Devices page, small fix in settings

---
+++ lib/devices/view/devices_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iot_anomaly_emulator/home/providers/counter_provider.dart';
+// import 'package:iot_anomaly_emulator/l10n/l10n.dart';
+
+class DevicesPage extends StatelessWidget {
+  const DevicesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CounterView();
+  }
+}
+
+class CounterView extends ConsumerWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final l10n = context.l10n;
+    final counter = ref.watch(counterProvider);
+    return Scaffold(
+      appBar: AppBar(title: Text('Current: $counter')),
+      body: const Center(child: CounterText()),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => ref.read(counterProvider.notifier).increment(),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            onPressed: () => ref.read(counterProvider.notifier).decrement(),
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterText extends StatelessWidget {
+  const CounterText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Text('0', style: theme.textTheme.displayLarge);
+  }
+}
