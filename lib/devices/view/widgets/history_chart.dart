74aaff094e17c9c3bc83cd3474beccf29b53bad5
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-08-27 19:01:38 +0200

    [FEA] Added labels to chart

--- lib/devices/view/widgets/history_chart.dart
+++ lib/devices/view/widgets/history_chart.dart
@@ -1,6 +1,7 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iot_anomaly_emulator/common/constants/sizes.dart';
 import 'package:iot_anomaly_emulator/devices/providers/sensor_history_provider.dart';
 
 class HistoryChart extends ConsumerWidget {
@@ -10,44 +11,78 @@ class HistoryChart extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final history = ref.watch(sensorHistoryProvider);
 
-    return SizedBox(
-      width: 400,
-      height: 400,
-      child: LineChart(
-        LineChartData(
-          lineBarsData: [
-            LineChartBarData(
-              color: Colors.red,
-              spots: [
-                for (int i = 0; i < history.length; i++)
-                  FlSpot(
-                    i.toDouble(),
-                    history[i].values['temperature']?.toDouble() ?? 0,
-                  ),
-              ],
-            ),
-            LineChartBarData(
-              color: Colors.yellow,
-              spots: [
-                for (int i = 0; i < history.length; i++)
-                  FlSpot(
-                    i.toDouble(),
-                    history[i].values['vibration']?.toDouble() ?? 0,
-                  ),
-              ],
-            ),
-            LineChartBarData(
-              spots: [
-                for (int i = 0; i < history.length; i++)
-                  FlSpot(
-                    i.toDouble(),
-                    history[i].values['pressure']?.toDouble() ?? 0,
-                  ),
-              ],
-            ),
-          ],
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: Sizes.p12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Temperature',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Vibration',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Pressure',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        SizedBox(
+          width: 400,
+          height: 320,
+          child: LineChart(
+            LineChartData(
+              lineBarsData: [
+                LineChartBarData(
+                  color: Colors.red,
+                  spots: [
+                    for (int i = 0; i < history.length; i++)
+                      FlSpot(
+                        i.toDouble(),
+                        history[i].values['temperature'] ?? 0,
+                      ),
+                  ],
+                ),
+                LineChartBarData(
+                  color: Colors.yellow,
+                  spots: [
+                    for (int i = 0; i < history.length; i++)
+                      FlSpot(
+                        i.toDouble(),
+                        history[i].values['vibration'] ?? 0,
+                      ),
+                  ],
+                ),
+                LineChartBarData(
+                  color: Colors.blue,
+                  spots: [
+                    for (int i = 0; i < history.length; i++)
+                      FlSpot(
+                        i.toDouble(),
+                        history[i].values['pressure'] ?? 0,
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
