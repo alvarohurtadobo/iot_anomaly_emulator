74e4c56130f47c279796c957068770f89c942f19
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-10-06 23:47:43 +0200

    [FIX] Removed no longer needed files

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,37 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:iot_anomaly_emulator/l10n/l10n.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CounterView();
-  }
-}
-
-class CounterView extends ConsumerWidget {
-  const CounterView({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = context.l10n;
-    return Scaffold(
-      body: Center(child: Text(l10n.welcome_text)),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              context.go('/devices');
-            },
-            child: const Icon(Icons.remove_red_eye),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
--- lib/settings/view/settings.dart
+++ lib/settings/view/settings.dart
@@ -1,55 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:iot_anomaly_emulator/devices/model/translate.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CounterView();
-  }
-}
-
-class CounterView extends ConsumerWidget {
-  const CounterView({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder<List<Translate>>(
-          future: getTranslations(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return const Text('Error loading');
-            } else if (snapshot.hasData &&
-                snapshot.data != null &&
-                snapshot.data!.isEmpty) {
-              return const Text('Empty data');
-            } else if (snapshot.hasData) {
-              final data = snapshot.data!;
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(data[index].id.toString()),
-                      Text(data[index].source),
-                      Text(data[index].target!),
-                    ],
-                  );
-                },
-              );
-            } else {
-              return const Text('Unknown error');
-            }
-          },
-        ),
-      ),
-    );
+    return Container();
   }
 }
