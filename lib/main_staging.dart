dc4d68d670dd36af27c04ee1b6ac9ad35303147e
Alvaro Hurtado <a.hurtado.bo@gmail.com>
2025-12-22 23:58:08 +0100

    chore improvements in launcher main

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -2,5 +2,5 @@ import 'package:iot_anomaly_emulator/app/app.dart';
 import 'package:iot_anomaly_emulator/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() async => const App());
 }
